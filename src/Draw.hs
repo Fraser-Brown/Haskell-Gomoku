6a6b2a70ba73b6144c71bf2da303cc23c26e0d81
taeh1 <taeh@st-andrews.ac.uk>
2018-03-22 00:42:28 +0000

    Started wireframe for drawWorld method in Draw.hs

--- src/Draw.hs
+++ src/Draw.hs
@@ -5,10 +5,31 @@ module Draw(drawWorld) where
 import Graphics.Gloss
 import Board
 
--- Given a world state, return a Picture which will render the world state.
+-- Given a world state, return a Picture which will render the world state. - https://hackage.haskell.org/package/gloss-1.1.0.0/docs/Graphics-Gloss-Picture.html
 -- Currently just draws a single blue circle as a placeholder.
+--
+-- World { board :: Board, turn :: Colour }
+--
+-- data Board = Board { size :: Int, target :: Int, pieces :: [(Position, Colour)]}
 --
 -- This will need to extract the Board from the world state and draw it
 -- as a grid plus pieces.
 drawWorld :: World -> Picture
-drawWorld w = Color blue $ Circle 100
+drawWorld w = do let board = board world
+                then do let go = show turn world
+                    then drawPicture world board turn
+
+-- http://andrew.gibiansky.com/blog/haskell/haskell-gloss/
+
+drawPicture :: World -> Board -> String -> Picture
+drawPicture world board turnstr
+
+drawNextPlayer :: String -> Picture
+
+drawTargetReminder :: Int -> Picture
+
+drawGrid :: Picture
+
+drawPieces :: [(Position, Colour)] -> Picture
+
+drawTitle :: Picture
