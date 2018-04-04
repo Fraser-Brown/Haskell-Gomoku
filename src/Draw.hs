9847b2d3f368032d056eb55682c5d1fed1d59518
assassinlemon <feb7@st-andrews.ac.uk>
2018-04-04 14:23:54 +0100

    can now be played with a grid, need to correct check win

--- src/Draw.hs
+++ src/Draw.hs
@@ -14,26 +14,26 @@ import Board
 --
 -- This will need to extract the Board from the world state and draw it
 -- as a grid plus pieces.
---drawWorld :: World -> Picture
-drawWorld w = display 
-(InWindow
-       "Gomoku"     -- window title
-        (1000, 700)       -- window size
-        (10, 10))        -- window position
-white                    -- background color
-gamePicture world              -- Picture object to draw, made of composite parts
+
+drawWorld :: World -> Picture
+drawWorld w = gamePicture w           
 
 gamePicture :: World -> Picture
 -- get composite parts to arrange combined Picture to display
-gamePicture world = Pictures [drawNextPlayer show turn board, drawTargetReminder target board, drawGrid size board, drawPieces pieces board size board, drawTitle]
+gamePicture world = Pictures [drawNextPlayer (show(turn world)), 
+                              --drawTargetReminder(target (board world)),
+                              drawGrid (size (board world)),
+                              drawPieces(pieces (board world)), 
+                              drawTitle,
+                              drawWinner (board world)]
 
 drawNextPlayer :: String -> Picture
 -- draw text informing the user of which player (Black/White) is next
-drawNextPlayer nPlyr = Text $ nPlyr ++ " has the next move"
+drawNextPlayer nPlyr = Translate (-150) 350 $ Scale 0.2 0.2 $ Text $ nPlyr ++ " has the next move"
 
-drawTargetReminder :: Int -> Picture
+--drawTargetReminder :: Int -> Picture
 -- draw text informing user of the target x in a row to get
-drawTargetReminder tgt = Text $ "Target: " ++ show tgt ++ " in a row"
+--drawTargetReminder tgt =  Translate -100 200 $ Scale 0.2 0.2 $ Text $ "Target: " ++ (show tgt) ++ " in a row"
 
 drawGrid :: Int -> Picture
 -- draw the lines of the game grid, where n is the grid width and height
@@ -45,21 +45,42 @@ grey = makeColor 122 122 122 1
 -- TODO: finish the next 2 functions to draw correctly positioned grid lines for the Gomoku board
 
 verticalLines :: Int -> Picture
-verticalLines noLines = Pictures [drawvertLine n noLines | n <- [1..noLines]]
-                        where drawvertLine lineNo noLines = grey Seg
+verticalLines noLines = pictures [drawvertline n noLines | n <- [0..noLines]]
+                        
+drawvertline :: Int -> Int -> Picture
+drawvertline x nolines = color red $ Line [(-250,(c *500) - 250),(250,(c * 500) -250)]
+                         where c = (fromIntegral(x)/fromIntegral(nolines))  
 
 horiLines :: Int -> Picture
-horiLines noLines = Pictures [drawhoriLine n noLines | n <- [1..noLines]]
-                        where drawhoriLine lineNo noLines = grey Seg
+horiLines noLines = pictures [drawhoriline n noLines | n <- [0..noLines]]
+                       
+drawhoriline :: Int -> Int -> Picture
+drawhoriline x nolines = color red $ Line [((c * 500) -250, -250) ,((c * 500) -250, 250)]
+                         where c = (fromIntegral(x)/fromIntegral(nolines)) 
 
-drawPieces :: [(Position, Colour)] -> Picture
+drawPieces :: [(Position, Col)] -> Picture
 -- draw each piece on the board
-drawPieces pieces = Pictures [drawPiece p | p <- pieces]
-                        where drawPiece p = if snd p == White then white Circle 2
-                                                              else black Circle 2
+drawPieces pieces = pictures [drawPiece p | p <- pieces] 
+
+drawPiece:: (Position, Col) -> Picture
+drawPiece p = if snd p == White then (Translate x y (color white (circleSolid 20)))
+                                else (Translate x y (color black (circleSolid 20)))
+                                where pos = fst p                               
+                                      x = fromIntegral(85  * (fst pos - 2)) - 42.5
+                                      y = fromIntegral(85 * (snd pos - 2)) - 42.5
+
+
 
 drawTitle :: Picture
 -- draw the title of the game
-drawTitle = Text "Gomoku"
+drawTitle =  Translate (-200) 400 $ Text ("Gomoku")
 
+drawWinner:: Board -> Picture
+drawWinner board = if winner == Nothing then Blank
+                                        else if maybeToCol(winner) == White then Text ("White Wins")
+                                             else Text("Black Wins")
+                  where winner = checkWon(board)  
+
+maybeToCol:: Maybe Col -> Col
+maybeToCol (Just x) = x
 --TODO: add Translate method to all methods drawing Picture elements, to correctly position them on the window
--- src/Input.hs
+++ src/Input.hs
@@ -16,16 +16,31 @@ import Debug.Trace
 -- 'trace' returns its second argument while printing its first argument
 -- to stderr, which can be a very useful way of debugging!
 handleInput :: Event -> World -> World
-handleInput (EventMotion (x, y)) b 
-    = trace ("Mouse moved to: " ++ show (x,y)) b
-handleInput (EventKey (MouseButton LeftButton) Up m (x, y)) b 
-    = trace ("Left button pressed at: " ++ show (x,y)) b
-handleInput (EventKey (Char k) Down _ _) b
-    = trace ("Key " ++ show k ++ " down") b
-handleInput (EventKey (Char k) Up _ _) b
-    = trace ("Key " ++ show k ++ " up") b
+--handleInput (EventMotion (x, y)) b  = trace ("Mouse moved to: " ++ show (x,y)) b
+
+handleInput (EventKey (MouseButton LeftButton) Up m (x, y)) b = {-trace ("Left button pressed at: " ++ show (x,y))-} newWorld
+                                                                where pos = getPos x y
+                                                                      ans = makeMove (board b) (turn b) pos 
+                                                                      newWorld = if nothingChecker(ans) == False then  b
+                                                                                                                 else World (maybeToBoard(ans)) (other(turn b))
+--handleInput (EventKey (Char k) Down _ _) b = trace ("Key " ++ show k ++ " down") b
+
+--handleInput (EventKey (Char k) Up _ _) b = trace ("Key " ++ show k ++ " up") b
+
 handleInput e b = b
 
+nothingChecker:: Maybe Board -> Bool
+nothingChecker Nothing = False
+nothingChecker (Just x) = True
+
+--to deal with the maybe issues
+maybeToBoard:: Maybe Board -> Board
+maybeToBoard (Just x) = x
+
+getPos:: Float -> Float -> Position
+getPos x y = (p, q)
+             where p = round(((x + 42)/85) + 2) --will need to change 3 if board gets bigger
+                   q = round(((y + 42)/85) + 2)
 --TODO: Implement appropriate event handlers for input events such as clicking on the board (above); these will need to identify which board position a mouse location maps to, in particular
 
 {- Hint: when the 'World' is in a state where it is the human player's
