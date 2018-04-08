cda8b4281d09f0023f9e738c55b322454100b47a
assassinlemon <feb7@st-andrews.ac.uk>
2018-04-08 15:35:34 +0100

    started on sgf input
    running windows so need to be in labs to full debug

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Graphics.Gloss
 import System.Environment
+import System.Directory
 
 import Board
 import Draw
@@ -35,12 +36,67 @@ main =do args <- getArgs
                updateWorld -- in MinimaxAI.hs
 
 makeWorld :: [String] -> World
-makeWorld args = World board Black 100 False
+makeWorld args | length args == 1 = fileReader(args!!0)
+               | otherwise = World board Black 100 False
                  where board = initBoard args 
                  
                  
 initBoard :: [String] -> Board
-initBoard args = if length args < 2 then Board 6 3 []
-                                    else Board (a) (b) []
-                                         where a = read(args !! 0) :: Int 
-                                               b = read(args !! 1) :: Int
+initBoard args | length args == 2  = Board (a) (b) []
+               | otherwise = Board 6 3 []
+               where a = read(args !! 0) :: Int 
+                     b = read(args !! 1) :: Int
+
+fileReader:: String -> World 
+fileReader file = do exists <- doesFileExist file
+                     if exists then
+                        do contents <- readFile(file)
+                           createFromFile (lines contents) 
+                     else do putStrLine "Cannot find file"
+                             World (Board 6 3 []) Black 100 False                         
+
+createFromFile :: [String] -> World
+createFromFile [] = putStrLine("The file was Empty") Board 6 3 []
+
+createFromFile inp = World x y 100 False
+                   where x = Board (findSize inp) (findTarget inp) (findPieces inp [])
+                         y = findPlayer inp           
+
+
+findTarget :: [String] -> Int
+findTarget (x:inp) | take 2 x == "TG" = read(filter(\y -> y.isDigit) x) :: Int
+                   | length inp > 0 = findTarget(inp)     
+                   | otherwise = 3
+
+findSize :: [String] -> Int
+findSize (x:inp) | take 2 x == "SZ" = read(filter(\y -> y.isDigit) x) :: Int
+                 | length inp > 0 = findTarget(inp)     
+                 | otherwise = 6
+
+findPlayer :: [String] -> Col 
+findSize (x:inp) | take 2 x == "PL" = if (x !! 3) == "B" then Black
+                                                         else White   
+                 | length inp > 0 = findTarget(inp)     
+                 | otherwise = 6
+
+findPieces :: [Strins] -> [(Position, Col)] -> [(Position, Col)] 
+findPieces [] r = r
+findPieces (x:inp) pos | take 2 x == "AW" = findPieces inp (pos ++ [(y), White]) 
+                       | take 2 x == "AB" = findPieces inp (pos ++ [(y), Black])  
+                       | length inp > 0 = findTarget(inp)     
+                       | otherwise = []
+                       where y = (a,b)
+                             jn = drop 3 x --to get just the positions 
+                             a = findA(jn, "")   
+                             b = findB(jn)
+
+findA:: String-> String -> Int 
+findA (x:inp) str | x == ',' = read(str) ::Int
+                  | x.isDigit = findA inp (str ++ x)
+                  | otherwise = if null inp then str
+                                             else findA inp str  
+
+findB:: String -> String ->Int
+findB (x:inp) | x == ',' = read(inp) ::Int
+              | otherwise = if null inp then -1
+                                        else findB inp 
