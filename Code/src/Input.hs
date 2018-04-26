b94484ae29ecf2d20fb0e911be24ffc245dc2ff5
taeh1 <taeh@st-andrews.ac.uk>
2018-04-26 10:35:51 +0100

    Refactored structure re the spec

---
+++ Code/src/Input.hs
@@ -0,0 +1,132 @@
+-- module to take input events, acting as an API to then call other functions in other modules to process them
+
+module Input(handleInputIO) where
+
+import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss
+import Board
+import AILogic
+import Draw
+
+-- Update the world state given an input event
+handleInputIO :: Event -> World -> IO World
+handleInputIO (EventKey (Char 's') Up _ _) world = saveGame(world)
+handleInputIO (EventKey (Char 'c') Up _ _) world = return newWorld 
+                                                 where newWorld = World (board world) (turn world) (timer world) (maxTimer world) (paused world) ("PVP")
+handleInputIO (EventKey (Char 'a') Up _ _) world = return newWorld 
+                                                 where newWorld = World (board world) (turn world) (timer world) (maxTimer world) (paused world) ("AI")    
+handleInputIO (EventKey (Char 't') Up _ _) world = return newWorld 
+                                                 where newWorld = World (board world) (turn world) (timer world) (maxTimer world) (paused world) ("TBT")
+handleInputIO (EventKey (Char 'f') Up _ _) world = return newWorld 
+                                                 where newWorld = World (largerB) (turn world) (timer world) (maxTimer world) (paused world) ("FBF") 
+                                                       b = board world
+                                                       largerB = Board (size b) (4) (pieces b)                                                                                                                                                   
+handleInputIO event world |(typeOfGame world) == "BLANK" = return world
+                          |otherwise = return $ handleInput event world
+ 
+
+saveGame:: World -> IO World
+saveGame world = do writeFile("SaveME.txt") (convertWorld(world))
+                    return world
+
+convertWorld:: World -> String
+convertWorld world = "TG "++ show (target (board world)) ++"\n" ++
+                     "SZ "++ show (size (board world)) ++"\n" ++  
+                     "TL "++ show (maxTimer world) ++"\n" ++
+                     "PL "++ (player) ++"\n" ++ allPieces
+                     where allPieces = printPieces(pieces(board world)) ("")
+                           player = if (turn world) == Black then "B"
+                                                             else "W"  
+                                                             
+printPieces:: [(Position, Col)] -> String -> String
+printPieces [] str = str
+printPieces (x:inp) str | snd x == Black = printPieces inp b
+                        | otherwise = printPieces inp w
+                        where b = str ++ "AB " ++ show(fst(fst x)) ++ "," ++ show(snd(fst x)) ++ "\n"  
+                              w = str ++ "AW " ++ show(fst(fst x)) ++ "," ++ show(snd(fst x)) ++ "\n"       
+
+handleInput :: Event -> World -> World
+
+handleInput (EventKey (MouseButton LeftButton) Up m (x, y)) b  | (typeOfGame b) == "TBT" = xGame b pos 3
+                                                               | (typeOfGame b) == "FBF" = xGame b pos 4   
+                                                               | otherwise = newWorld
+                                                                where pos = getPos x y (size (board b))
+                                                                      ans = makeMove (board b) (turn b) pos 
+                                                                      newWorld = if nothingChecker(ans) == False then  b
+                                                                                                                 else World (maybeToBoard(ans)) (other(turn b)) (maxTimer b) (maxTimer b) (False) (typeOfGame b)
+
+
+handleInput (EventKey (MouseButton RightButton) Up _ _) world = newWorld 
+                                                 where newWorld = World (Board (size b) (target b) (p)) (other(turn world)) (maxTimer world) (maxTimer world) (False) (typeOfGame world)
+                                                       b = (board world) 
+                                                       p = prevBoard (pieces b)
+                                                                                                                                                                                                    
+handleInput (EventKey (Char 'p') Up _ _) world = newWorld 
+                                                 where newWorld = World (board world) (other(turn world)) (timer world) (maxTimer world) (pause) (typeOfGame world)
+                                                       pause = if (paused world) then False
+                                                                                 else True   
+                                                                                                                                      
+
+handleInput(EventKey (SpecialKey KeyUp) Up _ _) world = newWorld 
+                                                      where newWorld = World (Board ((size b)+1) (target b) (pieces b)) (turn world) (timer world) (maxTimer world) (paused world) (typeOfGame world)
+                                                            b = board world
+
+
+handleInput(EventKey (SpecialKey KeyDown) Up _ _) world = newWorld 
+                                                      where newWorld = World newBoard (turn world) (timer world) (maxTimer world) (paused world) (typeOfGame world)
+                                                            b = board world
+                                                            newBoard = Board (newSize) (target b) (newPieces)
+                                                            newSize = if (size b) == 1 then 1
+                                                                                       else (size b) - 1 
+                                                            newPieces = removeOverflow (pieces b) [] newSize                            
+                                                                                       
+handleInput(EventKey (SpecialKey KeyRight) Up _ _) world = newWorld 
+                                                      where newWorld = World (Board (size b) ((target b) + 1) (pieces b)) (turn world) (timer world) (maxTimer world) (paused world) (typeOfGame world)
+                                                            b = board world
+
+
+handleInput(EventKey (SpecialKey KeyLeft) Up _ _) world = newWorld 
+                                                      where newWorld = World (Board (size b) (newTarget) (pieces b)) (turn world) (timer world) (maxTimer world) (paused world) (typeOfGame world)
+                                                            b = board world
+                                                            newTarget = if (target b) == 1 then 1
+                                                                                       else (target b) - 1                                                                                          
+ 
+                                                                                                                                             
+handleInput e b = b
+
+xGame:: World -> Position -> Int -> World
+xGame world pos t | checked == False =  world
+                  | validXMove (getAns(ans)) (turn world) t == False =  world
+                  | otherwise = World (maybeToBoard(ans)) (other(turn world)) (maxTimer world) (maxTimer world) (False) (typeOfGame world)
+                    where ans = makeMove (board world) (turn world) pos 
+                          checked = nothingChecker(ans)
+                        
+getAns:: Maybe Board -> Board
+getAns (Just x) = x
+
+removeOverflow:: [(Position, Col)]-> [(Position, Col)] -> Int -> [(Position, Col)]
+removeOverflow [] pieces _ = pieces
+removeOverflow (x:inp) pieces s | fst pos >= s = removeOverflow inp pieces s 
+                                | snd pos >= s = removeOverflow inp pieces s
+                                | otherwise = removeOverflow inp (pieces ++ [x]) s   
+                                where pos = fst x    
+                  
+prevBoard:: [(Position, Col)] ->  [(Position, Col)]
+prevBoard [] = []
+prevBoard p = init(p)
+
+nothingChecker:: Maybe Board -> Bool
+nothingChecker Nothing = False
+nothingChecker (Just x) = True
+
+getPos:: Float -> Float -> Int -> Position
+getPos x y size = (p, q)
+             where p = floor((x+250) / (500/fromIntegral(size)))
+                   q = floor((y+250) / (500/fromIntegral(size)))
+
+{- Hint: when the 'World' is in a state where it is the human player's
+ turn to move, a mouse press event should calculate which board position
+ a click refers to, and update the board accordingly.
+
+ At first, it is reasonable to assume that both players are human players.
+-}
---
+++ Code/src/Main.hs
@@ -0,0 +1,122 @@
+-- contains the overall control of the game and how graphics, event handlers etc. are initialised
+
+module Main where
+
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+
+import System.Environment
+import System.Directory
+import Data.Char
+
+import Board
+import Draw
+import Input
+import AILogic
+
+-- 'play' starts up a graphics window and sets up handlers for dealing
+-- with inputs and updating the world state.
+--
+-- 'drawWorld' converts the world state into a gloss Picture
+--
+-- 'handleInput' is called whenever there is an input event, and if it is
+-- a human player's turn should update the board with the move indicated by
+-- the event
+--
+-- 'updateWorld' is called 10 times per second (that's the "10" parameter)
+-- and, if it is an AI's turn, should update the board with an AI generated
+-- move
+
+
+
+
+main :: IO ()
+main =do args <- getArgs
+         world <- readWorld args
+
+         playIO (InWindow "Gomoku" (1200, 1000) (100,100)) yellow 10
+               world
+               drawWorld -- in Draw.hs
+               handleInputIO -- in Input.hs
+               updateWorld -- in AI.hs
+         
+
+
+
+defaultWorld = World (initBoard []) Black 100 100 False "BLANK"
+
+readWorld :: [String] -> IO World
+readWorld [] = return defaultWorld
+readWorld args = case args of
+                  [] -> return defaultWorld
+                  strings -> do exists <- doesFileExist(args!!0)
+                                case exists of
+                                     False -> return (cmdLineWorld args)
+                                     True -> do contents <- readFile(args!!0)
+                                                case contents of
+                                                   [] -> return defaultWorld
+                                                   strings -> return (createFromFile(lines strings))
+
+
+cmdLineWorld::[String] -> World     
+cmdLineWorld args = World b Black 100 100 False "BLANK" 
+                  where b = Board s t []
+                        s = read(args!!0) :: Int
+                        t = read(args!!1) :: Int                                         
+
+createFromFile :: [String] -> World
+createFromFile [] = World (initBoard []) Black 100 100 False "BLANK"
+
+createFromFile inp = World (x) (y) (findTimeLimt(inp)) (findTimeLimt(inp)) (False) "BLANK" 
+                     where x = (Board (findSize inp) (findTarget inp) (findPieces inp []))
+                           y = (findPlayer inp)           
+
+
+
+---------------------------------------------------------------------------------------------------
+
+
+findTarget :: [String] -> Int
+findTarget (x:inp) | take 2 x == "TG" = read(filter(\y -> isDigit(y)) x) :: Int
+                   | length inp > 0 = findTarget(inp)     
+                   | otherwise = 4
+
+findSize :: [String] -> Int
+findSize (x:inp) | take 2 x == "SZ" = read(filter(\y -> isDigit(y)) x) :: Int
+                 | length inp > 0 = findSize(inp)     
+                 | otherwise = 7
+
+findTimeLimt :: [String] -> Int
+findTimeLimt (x:inp) | take 2 x == "TL" = read(filter(\y -> isDigit(y)) x) :: Int
+                     | length inp > 0 = findTimeLimt(inp)     
+                     | otherwise = 100                 
+
+
+findPlayer :: [String] -> Col 
+findPlayer (x:inp) | take 2 x == "PL" = if (x !! 3) == 'B' then Black
+                                                           else White   
+                   | length inp > 0 = findPlayer(inp)     
+                   | otherwise = Black
+
+findPieces :: [String] -> [(Position, Col)] -> [(Position, Col)]  --done this way to preserve move order, so that the undo will still work (potential for learning ai also)
+findPieces [] r = r
+findPieces (x:inp) pos | take 2 x == "AW" = findPieces inp (pos ++ [((y), White)]) 
+                       | take 2 x == "AB" = findPieces inp (pos ++ [((y), Black)])  
+                       | length inp > 0 = findPieces (inp) pos    
+                       | otherwise = []
+                       where y = (a,b)
+                             jn = drop 3 x --to get just the positions 
+                             a = read(findA jn "") :: Int   
+                             b = read(findB(jn)) ::Int
+
+findA:: String -> String -> String 
+findA (x:inp) str | x == ',' = str
+                  | isDigit(x) = findA inp (str ++ [x])
+                  | otherwise = if null inp then str
+                                             else findA inp str  
+
+findB:: String -> String
+findB (x:inp) | x == ',' = (filter(\y -> isDigit(y)) inp)
+              | length inp > 0 = findB inp
+              | otherwise = "-1"
+                              
